0c26f5c935e40c8750b964bab35e63343cecd525
Lucasmercado101 <lucasmercado101@gmail.com>
2021-07-25 18:49:14 -0300

    converted electron.js to .ts

--- electron/electron.js
+++ electron/electron.js
@@ -1,206 +1,112 @@
-const {
-  app,
-  BrowserWindow,
-  Menu,
-  globalShortcut,
-  ipcMain,
-  dialog
-} = require("electron");
-// const { observable } = require("mobx");
-const {
-  createFile,
-  dirOrFileExists,
-  readFile,
-  createFolderIfNotExists,
-  readDir,
-  createFolder
-} = require("./helpers.js");
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
 const path = require("path");
-const fs = require("fs");
-const userProfilesPath = path.join(app.getPath("userData"), "profiles");
+const helpers_1 = require("./helpers");
 const isDev = require("electron-is-dev");
-
+const userProfilesPath = path.join(electron_1.app.getPath("userData"), "profiles");
 try {
-  isDev && require("electron-reloader")(module);
-} catch (_) {}
-
-/**
- * @typedef defaultUserData
- * @property {600} timeLimitInSeconds
- */
-
-/**
- * @type {defaultUserData}
- */
-const defaultUserSettings = { timeLimitInSeconds: 600 };
-
-/**
- * TODO another listener to load saved records / history
- * TODO also load custom user lessons in this listener
- *
- * * ---- Loads user profile data (settings) ----
- *
- * * Creates user profile folder if it does not exist
- * * Creates user's settings.json if it does not exist
- * * Loads user's settings.json or the default if it doesn't exist
- *
- * NOTE It's assumed in this listener that the user profiles folder exists.
- *      user profiles folder is created at:
- * LINK ./electron.js#load-user-profiles-names-listener
- */
-ipcMain.handle(
-  "load-user-data",
-  /**
-   * @param {string} userName
-   * @returns {Promise<any | undefined>}
-   */
-  async (_, userName) => {
-    const userFolderPath = path.join(userProfilesPath, userName);
-    const userSettingsPath = path.join(
-      userProfilesPath,
-      userName,
-      "settings.json"
-    );
-
-    try {
-      await createFolderIfNotExists(userFolderPath);
-    } catch (err) {
-      dialog.showErrorBox("Error", err.message);
-      return undefined;
-    }
-
-    const userSettingsExists = await dirOrFileExists(userSettingsPath);
-
-    if (!userSettingsExists) {
-      return createFile(userSettingsPath, JSON.stringify(defaultUserSettings))
-        .then(() => {
-          return defaultUserSettings;
-        })
-        .catch(
-          /**
-           * @param {NodeJS.ErrnoException} err
-           */
-          (err) => {
-            dialog.showErrorBox("Error", err.message);
-            return undefined;
-          }
-        );
-    } else {
-      return readFile(userSettingsPath).catch(
-        /**
-         * @param {NodeJS.ErrnoException} err
-         */
-        (err) => {
-          dialog.showErrorBox("Error", err.message);
-          return undefined;
-        }
-      );
-    }
-  }
-);
-
-/**
- * ANCHOR[id=load-user-profiles-names-listener]
- *
- * * ---- Return user profile names as a string array ----
- *
- * * if an error occurs at any moment, it shows an error dialog box
- * * and returns "undefined"
- *
- */
-ipcMain.handle("load-user-profiles-names", async () => {
-  const userProfilesFolderExists = await dirOrFileExists(userProfilesPath);
-
-  if (userProfilesFolderExists)
-    return await readDir(userProfilesPath).catch(
-      /**
-       * @param {NodeJS.ErrnoException} err
-       */
-      (err) => {
-        dialog.showErrorBox("Error", err.message);
-        return undefined;
-      }
-    );
-  else
-    return createFolder(userProfilesPath)
-      .then(() => [])
-      .catch(
-        /**
-         * @param {NodeJS.ErrnoException} err
-         */
-        (err) => {
-          dialog.showErrorBox("Error", err.message);
-          return undefined;
-        }
-      );
-});
-
-function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 580,
-    minWidth: 800,
-    minHeight: 580,
-    resizable: isDev ? true : false,
-    title: "MecaMatic 3.0",
-    webPreferences: {
-      nodeIntegration: true,
-      enableRemoteModule: true,
-      contextIsolation: false
-    }
-  });
-  win.loadURL(
-    "http://localhost:1234"
-    // TODO production files}
-    // `file://${path.join(__dirname, "./index.html")}`
-  );
-  // isDev
-  //   ? "http://localhost:3000"
-  //   : `file://${path.join(__dirname, "../build/index.html")}`
-
-  if (isDev) win.webContents.openDevTools();
-
-  const menu = Menu.buildFromTemplate([
-    {
-      label: "Eliminar Usuario",
-      click() {
-        win.webContents.send("get-selected-user");
-      }
-    }
-  ]);
-  Menu.setApplicationMenu(menu);
+    isDev && require("electron-reloader")(module);
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app
-  .whenReady()
-  .then(() => {
-    if (isDev) {
-      globalShortcut.register("CommandOrControl+Shift+C", () => {
-        const win = BrowserWindow.getFocusedWindow();
-        win && win.webContents.openDevTools();
-      });
+catch (_) { }
+const defaultUserSettings = { timeLimitInSeconds: 600 };
+electron_1.ipcMain.handle("load-user-data", (_, userName) => __awaiter(void 0, void 0, void 0, function* () {
+    const userFolderPath = path.join(userProfilesPath, userName);
+    const userSettingsPath = path.join(userProfilesPath, userName, "settings.json");
+    try {
+        yield helpers_1.createFolderIfNotExists(userFolderPath);
     }
-  })
-  .then(createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+    catch (err) {
+        electron_1.dialog.showErrorBox("Error", err.message);
+        return undefined;
+    }
+    const userSettingsExists = yield helpers_1.dirOrFileExists(userSettingsPath);
+    if (!userSettingsExists) {
+        return helpers_1.createFile(userSettingsPath, JSON.stringify(defaultUserSettings))
+            .then(() => {
+            return defaultUserSettings;
+        })
+            .catch((err) => {
+            electron_1.dialog.showErrorBox("Error", err.message);
+            return undefined;
+        });
+    }
+    else {
+        return helpers_1.readFile(userSettingsPath).catch((err) => {
+            electron_1.dialog.showErrorBox("Error", err.message);
+            return undefined;
+        });
+    }
+}));
+electron_1.ipcMain.handle("load-user-profiles-names", () => __awaiter(void 0, void 0, void 0, function* () {
+    const userProfilesFolderExists = yield helpers_1.dirOrFileExists(userProfilesPath);
+    if (userProfilesFolderExists)
+        return yield helpers_1.readDir(userProfilesPath).catch((err) => {
+            electron_1.dialog.showErrorBox("Error", err.message);
+            return undefined;
+        });
+    else
+        return helpers_1.createFolder(userProfilesPath)
+            .then(() => [])
+            .catch((err) => {
+            electron_1.dialog.showErrorBox("Error", err.message);
+            return undefined;
+        });
+}));
+function createWindow() {
+    const win = new electron_1.BrowserWindow({
+        width: 800,
+        height: 580,
+        minWidth: 800,
+        minHeight: 580,
+        resizable: isDev ? true : false,
+        title: "MecaMatic 3.0",
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false
+        }
+    });
+    win.loadURL("http://localhost:1234");
+    if (isDev)
+        win.webContents.openDevTools();
+    const menu = electron_1.Menu.buildFromTemplate([
+        {
+            label: "Eliminar Usuario",
+            click() {
+                win.webContents.send("get-selected-user");
+            }
+        }
+    ]);
+    electron_1.Menu.setApplicationMenu(menu);
+}
+electron_1.app
+    .whenReady()
+    .then(() => {
+    if (isDev) {
+        electron_1.globalShortcut.register("CommandOrControl+Shift+C", () => {
+            const win = electron_1.BrowserWindow.getFocusedWindow();
+            win && win.webContents.openDevTools();
+        });
+    }
+})
+    .then(createWindow);
+electron_1.app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        electron_1.app.quit();
+    }
 });
-
-app.on("activate", () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+electron_1.app.on("activate", () => {
+    if (electron_1.BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
---
+++ electron/electron.ts
@@ -0,0 +1,205 @@
+import {
+  app,
+  BrowserWindow,
+  Menu,
+  globalShortcut,
+  ipcMain,
+  dialog
+} from "electron";
+import * as path from "path";
+import {
+  createFile,
+  dirOrFileExists,
+  readFile,
+  createFolderIfNotExists,
+  readDir,
+  createFolder
+} from "./helpers";
+import * as isDev from "electron-is-dev";
+
+const userProfilesPath = path.join(app.getPath("userData"), "profiles");
+
+try {
+  isDev && require("electron-reloader")(module);
+} catch (_) {}
+
+/**
+ * @typedef defaultUserData
+ * @property {600} timeLimitInSeconds
+ */
+
+/**
+ * @type {defaultUserData}
+ */
+const defaultUserSettings = { timeLimitInSeconds: 600 };
+
+/**
+ * TODO another listener to load saved records / history
+ * TODO also load custom user lessons in this listener
+ *
+ * * ---- Loads user profile data (settings) ----
+ *
+ * * Creates user profile folder if it does not exist
+ * * Creates user's settings.json if it does not exist
+ * * Loads user's settings.json or the default if it doesn't exist
+ *
+ * NOTE It's assumed in this listener that the user profiles folder exists.
+ *      user profiles folder is created at:
+ * LINK ./electron.js#load-user-profiles-names-listener
+ */
+ipcMain.handle(
+  "load-user-data",
+  /**
+   * @param {string} userName
+   * @returns {Promise<any | undefined>}
+   */
+  async (_, userName) => {
+    const userFolderPath = path.join(userProfilesPath, userName);
+    const userSettingsPath = path.join(
+      userProfilesPath,
+      userName,
+      "settings.json"
+    );
+
+    try {
+      await createFolderIfNotExists(userFolderPath);
+    } catch (err) {
+      dialog.showErrorBox("Error", err.message);
+      return undefined;
+    }
+
+    const userSettingsExists = await dirOrFileExists(userSettingsPath);
+
+    if (!userSettingsExists) {
+      return createFile(userSettingsPath, JSON.stringify(defaultUserSettings))
+        .then(() => {
+          return defaultUserSettings;
+        })
+        .catch(
+          /**
+           * @param {NodeJS.ErrnoException} err
+           */
+          (err) => {
+            dialog.showErrorBox("Error", err.message);
+            return undefined;
+          }
+        );
+    } else {
+      return readFile(userSettingsPath).catch(
+        /**
+         * @param {NodeJS.ErrnoException} err
+         */
+        (err) => {
+          dialog.showErrorBox("Error", err.message);
+          return undefined;
+        }
+      );
+    }
+  }
+);
+
+/**
+ * ANCHOR[id=load-user-profiles-names-listener]
+ *
+ * * ---- Return user profile names as a string array ----
+ *
+ * * if an error occurs at any moment, it shows an error dialog box
+ * * and returns "undefined"
+ *
+ */
+ipcMain.handle("load-user-profiles-names", async () => {
+  const userProfilesFolderExists = await dirOrFileExists(userProfilesPath);
+
+  if (userProfilesFolderExists)
+    return await readDir(userProfilesPath).catch(
+      /**
+       * @param {NodeJS.ErrnoException} err
+       */
+      (err) => {
+        dialog.showErrorBox("Error", err.message);
+        return undefined;
+      }
+    );
+  else
+    return createFolder(userProfilesPath)
+      .then(() => [])
+      .catch(
+        /**
+         * @param {NodeJS.ErrnoException} err
+         */
+        (err) => {
+          dialog.showErrorBox("Error", err.message);
+          return undefined;
+        }
+      );
+});
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 580,
+    minWidth: 800,
+    minHeight: 580,
+    resizable: isDev ? true : false,
+    title: "MecaMatic 3.0",
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+      contextIsolation: false
+    }
+  });
+  win.loadURL(
+    "http://localhost:1234"
+    // TODO production files}
+    // `file://${path.join(__dirname, "./index.html")}`
+  );
+  // isDev
+  //   ? "http://localhost:3000"
+  //   : `file://${path.join(__dirname, "../build/index.html")}`
+
+  if (isDev) win.webContents.openDevTools();
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: "Eliminar Usuario",
+      click() {
+        win.webContents.send("get-selected-user");
+      }
+    }
+  ]);
+  Menu.setApplicationMenu(menu);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app
+  .whenReady()
+  .then(() => {
+    if (isDev) {
+      globalShortcut.register("CommandOrControl+Shift+C", () => {
+        const win = BrowserWindow.getFocusedWindow();
+        win && win.webContents.openDevTools();
+      });
+    }
+  })
+  .then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
